ba744d408012023659462943f4ec88c5ef616dbb
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-09-16 22:15:21 +0200

    Fixed filter docstring for class

--- src/filter.py
+++ src/filter.py
@@ -1,35 +1,34 @@
-"""
-This is simple low-pass filter of 1st order. Transfer function in s-domain is:
-
-               1
-    G(s) = ----------
-           (1 + Tf*s)
-
-    Tf - filter time constant
-
-Discrete transformation of that functions, by approximating "s" with
-2*(z-1) / T*(z+1) is:
-
-              T                       T - T*Tf
-    y(k) = -------- (x(k) + x(k-1)) - -------- y(k-1)
-           T + 2*Tf                   T + 2*Tf
-
-    y(k)   - current output value
-    x(k)   - current input value
-    x(k-1) - previous input value
-    T      - sample time period
-
-Or in short:
-            A                     B
-    y(k) = --- (x(k) + x(k-1)) - --- y(k-1)  =  P * (x(k) + x(k-1)) - O * y(k-1)
-            C                     C
-
-Additional notes:
-- "x(k-1)", for example, is named as a variable as "x_1"
-- smaller the T constant, more accurate the output becomes
-"""
-
 class Filter:
+    """
+    This is simple low-pass filter of 1st order. Transfer function in s-domain is:
+
+                1
+        G(s) = ----------
+            (1 + Tf*s)
+
+        Tf - filter time constant
+
+    Discrete transformation of that functions, by approximating "s" with
+    2*(z-1) / T*(z+1) is:
+
+                T                       T - T*Tf
+        y(k) = -------- (x(k) + x(k-1)) - -------- y(k-1)
+            T + 2*Tf                   T + 2*Tf
+
+        y(k)   - current output value
+        x(k)   - current input value
+        x(k-1) - previous input value
+        T      - sample time period
+
+    Or in short:
+                A                     B
+        y(k) = --- (x(k) + x(k-1)) - --- y(k-1)  =  P * (x(k) + x(k-1)) - O * y(k-1)
+                C                     C
+
+    Additional notes:
+    - "x(k-1)", for example, is named as a variable as "x_1"
+    - smaller the T constant, more accurate the output becomes
+    """
 
     def __init__(self, T, Tf):
         self.__calcConstants(T, Tf)
