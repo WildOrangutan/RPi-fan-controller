d802762cbdaea8ffcf3ebabdda3d89447ca94e88
Peter Mandeljc <WildOrangutan@users.noreply.github.com>
2020-09-26 00:20:58 +0200

    Added TestCheck _checkRangeErr()

--- tests/test_check.py
+++ tests/test_check.py
@@ -11,19 +11,17 @@ class TestCheck(unittest.TestCase):
         self._checkRange(-1, min=-1, max=-1)
         self._checkRange(-1, min=-2, max=0)
         # Out of range
-        with self.assertRaises(ValueError):
-            self._checkRange(0, min=1)
-        with self.assertRaises(ValueError):
-            self._checkRange(-2, min=-1)
-        with self.assertRaises(ValueError):
-            self._checkRange(1, max=0)
-        with self.assertRaises(ValueError):
-            self._checkRange(-1, max=-2)
+        self._checkRangeErr(0, min=1)
+        self._checkRangeErr(-2, min=-1)
+        self._checkRangeErr(1, max=0)
+        self._checkRangeErr(-1, max=-2)
         # Invalid min max
+        self._checkRangeErr(0, min=2, max=1)
+        self._checkRangeErr(0, -2, -3)
+    
+    def _checkRangeErr(self, value, min=None, max=None):
         with self.assertRaises(ValueError):
             self._checkRange(0, min=2, max=1)
-        with self.assertRaises(ValueError):
-            self._checkRange(0, -2, -3)
 
     def _checkRange(self, value, min=None, max=None):
         check.range("Fart", value, min, max)
